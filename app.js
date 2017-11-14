120334bf152fe6539956d1617dac1ec4ae66da1b
“your_u <996468104@qq.com>
2017-11-14 16:42:00 +0800

    d

--- app.js
+++ app.js
@@ -99,7 +99,7 @@ mongoose.connect('mongodb://localhost:27018/blog',{useMongoClient: true},functio
         console.log("数据库连接失败！");
     }else{
         console.log("数据库连接成功！");
-        app.listen($PORT);
+        app.listen(PORT);
     }
 });
 
